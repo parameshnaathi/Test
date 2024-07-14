a3e671442afff37ce44a272b7e33452b56569100
parameshnaathi <eeshwar2424@gmail.com>
2024-07-14 13:48:30 +0530

    Delete scripts/scripts/stop_container.sh       timeout: 300       runas: root   AfterInstall:     - location: scripts directory

--- scripts/scripts/stop_container.sh       timeout: 300       runas: root   AfterInstall:     - location: scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker push parameshnaathi/simple-python-flask-app
-
-# Run the Docker image as a container
-docker -d -p 5000:5000 parameshnaathi/simple-python-flask-app
